44c243362707f6ff8c175a43b37aa6b7d7ec4091
andris1177 <43418048+andris1177@users.noreply.github.com>
2022-12-02 20:10:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#import
 import pygame
 import random
 
-#függvények
 def ball_animation():
     global ball_speed_x, ball_speed_y, player_score, opponent_score
 
@@ -50,7 +48,6 @@ def ball_reset():
     ball_speed_y *= random.choice((1, -1))
     ball_speed_x *= random.choice((1, -1))
 
-#változók
 height = 500
 width = 900
 black = (0, 0, 0)
@@ -58,37 +55,29 @@ white = (255, 255, 255)
 speed = 10
 fps = 60
 
-#pontok
 player_score = 0
 opponent_score = 0
 
-#labda
 ball_speed_x = 7 * random.choice((1, -1))
 ball_speed_y = 7 * random.choice((1, -1))
 player_speed = 0
 vona_speed = 7
 
-#pygame inicializálása
 pygame.init()
 pygame.font.init()
 
-#az óra beállítása fps korlátozás miatt
 clock = pygame.time.Clock()
 
-#az eredmény kiírása
 text_font = pygame.font.SysFont("arial.ttf", 50)
 
-#ablak létrehozás és title
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong")
 
-#kijelzőre rajzolt elemek
 ball = pygame.Rect(width/2 -15, height/2 -15, 30, 30)
 player = pygame.Rect(width -20, height /2 -70, 10, 140)
 opponent = pygame.Rect(10,height/2 - 70, 10, 140)
 
 
-#fő ciklus
 running = True
 while running:
     player_score_string = str(player_score)
@@ -97,38 +86,28 @@ while running:
     opponent_score_string = str(opponent_score)
     text_opponent = text_font.render(opponent_score_string, 1, white)
 
-    #figyeli a billentyúzet-et
     for event in pygame.event.get():
-        #kilépés
         if event.type == pygame.QUIT:
             running = False
 
-        #ha a billenytű le van nyomvba
         if event.type == pygame.KEYDOWN:
-            #le
             if event.key == pygame.K_DOWN:
                 player_speed += 7
 
-            #fel
             if event.key == pygame.K_UP:
                 player_speed -= 7
 
-        #ha nincs lenyomva
         if event.type == pygame.KEYUP:
-            #nem menjen tovább
             if event.key == pygame.K_DOWN:
                 player_speed -= 7
 
-            #nem menjen tovább
             if event.key == pygame.K_UP:
                 player_speed += 7
 
-    #függvények meghívása
     ball_animation()
     player_animation()
     opponent_animation()
 
-    #kijelző kitőltése krakterekkel
     screen.fill(black)
     screen.blit(text_player, (width/2 +40, 0))
     screen.blit(text_opponent, (width/2 -60, 0))
@@ -137,11 +116,8 @@ while running:
     pygame.draw.ellipse(screen, white, ball)
     pygame.draw.aaline(screen, white, (width/2, 0), (width/2, height))
 
-    #kijelzőre rajzolás
     pygame.display.flip()
 
-    #fps beállítása 60-ra
     clock.tick(fps)
 
-#kilép a pygame-ből
 pygame.quit()
